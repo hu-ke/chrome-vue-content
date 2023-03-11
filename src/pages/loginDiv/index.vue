a4763eea36aa2ebe9bf2c16f9c326ee2127a4a18
HU KE <403392669@qq.com>
2023-03-11 21:54:18 +0800

    refactor: drag

--- src/pages/loginDiv/index.vue
+++ src/pages/loginDiv/index.vue
@@ -1,6 +1,7 @@
 <template>
-    <div id="maskDrag" class="ui-widget-content" v-if="iframeVisible">
+    <div id="maskDrag" class="ui-widget-content" v-if="iframeVisible" @mousedown="move">
         <div style="position: relative;">
+            <!-- 因为iframe把maskDrag盖住了，所以拖拽在header-circle上才有效 -->
             <div class="header-circle"></div>
             <!-- <iframe id="iframe-wishlist" height="720" width="375" title="three part login"></iframe> -->
             <iframe style="border: none;" id="iframe-wishlist" height="720" :width="iframeWishlistWidth || 375" src="http://localhost:5173/plugin/home" title="three part login"></iframe>
@@ -23,6 +24,30 @@ export default defineComponent({
         const data = reactive({
 
         })
+
+        const move = (e: MouseEvent) => {
+          const maskDragEle = document.getElementById('maskDrag') as any
+          let disX = e.clientX - maskDragEle?.offsetLeft
+          let disY = e.clientY - maskDragEle?.offsetTop
+          console.log('disX', disX, 'disY', disY)
+          document.onmousemove = (e)=>{       //鼠标按下并移动的事件
+                //用鼠标的位置减去鼠标相对元素的位置，得到元素的位置
+                let left = e.clientX - disX;    
+                let top = e.clientY - disY;
+                
+                //绑定元素位置到positionX和positionY上面
+                // this.positionX = top;
+                // this.positionY = left;
+                
+                //移动当前元素
+                maskDragEle.style.left = left + 'px';
+                maskDragEle.style.top = top + 'px';
+            };
+            document.onmouseup = (e) => {
+                document.onmousemove = null;
+                document.onmouseup = null;
+            };
+        }
         onMounted(async() => {
             // await chrome.runtime.sendMessage({ operation: 'getEnv' }, async function (res) {
             //     let url = res ? `${res}/plugin/wishlist` : 'https://getguang.com/plugin/wishlist'
@@ -45,6 +70,7 @@ export default defineComponent({
             // });
         })
         return {
+          move,
           ...toRefs(data)
         }
     }
@@ -65,6 +91,14 @@ export default defineComponent({
   border-radius: 32px;
   // @ts-ignore
   box-shadow: 0 4px 7px RGB(30 41 59 / 20%);
+  // 不要高亮元素
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -o-user-select: none;
+  user-select: none;
 }
 
 .close-drag {
